db687615ebd6777e31090a47fb714be1f0d51bd2
idledeity <idledeity@jumpjets.net>
2017-06-27 23:26:03 -0500

    Adding a new Object class and moving the rock's definition into data

--- js/game.js
+++ js/game.js
@@ -6,11 +6,14 @@ Game.init = function(){
 };
 
 Game.preload = function() {
-    game.load.image('rock','assets/sprites/rock.png'); // this will be the sprite of the players
+    game.load.pack('rock', 'assets/packs/rock.json', null, this);
 };
 
 Game.create = function(){
     game.stage.backgroundColor = "#4488AA";
+
+    // Store a reference to the phaser game instance
+    Game.phaserGame = game;
 
     // Create the game's physics
     Game.physics = {};
@@ -29,77 +32,37 @@ Game.create = function(){
         }
     });
 
+    Game.objects = [];
+
     // Create a static physics shape for the ground
     Game.ground = Matter.Bodies.rectangle(500, 800, 8100, 600, { isStatic: true });
     Matter.World.add(Game.physics.engine.world, Game.ground);
 
     // Create the rock
-    Game.rock = {};
-    Game.rock.collisionBody = Matter.Bodies.fromVertices(200, 100, Matter.Vertices.create([
-        { x: 4, y: 75 },
-        { x: 2, y: 73 },
-        { x: 0, y: 67 },
-        { x: 0, y: 54 },
-        { x: 2, y: 46 },
-        { x: 6, y: 34 },
-        { x: 8, y: 22 },
-        { x: 10, y: 18 },
-        { x: 12, y: 16 },
-        { x: 14, y: 12 },
-        { x: 18, y: 8 },
-        { x: 23, y: 8 },
-        { x: 30, y: 4 },
-        { x: 40, y: 2 },
-        { x: 44, y: 0 },
-        { x: 73, y: 0 },
-        { x: 83, y: 4 },
-        { x: 85, y: 6 },
-        { x: 87, y: 18 },
-        { x: 89, y: 24 },
-        { x: 91, y: 28 },
-        { x: 99, y: 36 },
-        { x: 101, y: 40 },
-        { x: 103, y: 46 },
-        { x: 103, y: 61 },
-        { x: 95, y: 69 },
-        { x: 32, y: 69 },
-        { x: 31, y: 73 },
-        { x: 29, y: 75 }
-    ]));
-    Matter.World.add(Game.physics.engine.world, Game.rock.collisionBody);
-    Game.rock.sprite = game.add.sprite(100, 200,'rock');
+    Game.rock = new GameObject("objects/rock.json", this, 50, 50);
+    Game.objects.push(Game.rock);
+
+    // Enable input handling on the rock
     Game.rock.sprite.inputEnabled = true;
     Game.rock.sprite.events.onInputDown.add(Game.handleRockClicked, this);
     Game.rock.sprite.events.onInputUp.add(Game.handleRockUnclicked, this);
     
-    // Adjust the anchor of the sprite to the center of mass of the collision body
-    let centerOfMassOffset = Matter.Vector.sub(Game.rock.collisionBody.position, Game.rock.collisionBody.bounds.min);
-    let boundingExtends = Matter.Vector.sub(Game.rock.collisionBody.bounds.max, Game.rock.collisionBody.bounds.min);
-    let anchorX = centerOfMassOffset.x / boundingExtends.x;
-    let anchorY = centerOfMassOffset.y / boundingExtends.y;
-    Game.rock.sprite.anchor.setTo(anchorX, anchorY);
-
     // Give the rock an initial kick to make things interesting
-    Matter.Body.setAngularVelocity(Game.rock.collisionBody, 0.35);
+    Matter.Body.setAngularVelocity(Game.rock.rigidBody, 0.35);
 };
 
 Game.update = function() {
     // Update physics
     Matter.Engine.update(Game.physics.engine, game.time.physicsElapsedMS);
 
-    // Move the sprite coordinates to the physics coordinates
-    Game.rock.sprite.x = Game.rock.collisionBody.position.x;
-    Game.rock.sprite.y = Game.rock.collisionBody.position.y;
-    Game.rock.sprite.angle = Game.rock.collisionBody.angle * 180 / 3.14;
+    // Update objects
+    for (objectIdx = 0; objectIdx < Game.objects.length; objectIdx++) {
+        Game.objects[objectIdx].update();
+    }
 
     // Update the force line
     if (Game.rockClicked) {
-        let localPos = new Phaser.Point(Game.rockClickedPos.x, Game.rockClickedPos.y);
-        localPos.subtract(Game.rock.sprite.offsetX, Game.rock.sprite.offsetY);
-        let rotationMatrix = new Phaser.Matrix();
-        rotationMatrix.rotate(Game.rock.sprite.rotation);
-        let transformPos = rotationMatrix.apply(localPos);
-        let worldPos = Phaser.Point.add(transformPos, Game.rock.sprite.position);
+        let worldPos = Game.rock.getWorldFromLocal(Game.rockClickedPos.x, Game.rockClickedPos.y);
 
         Game.forceLine.setTo(worldPos.x, worldPos.y, Game.input.mousePointer.x, Game.input.mousePointer.y)
     }
@@ -116,21 +79,13 @@ Game.render = function() {
 
 Game.handleRockClicked = function(clickedObj, pointer) {
     // Do a finer check to see if the rock was actually clicked
-    if (!Matter.Vertices.contains(Game.rock.collisionBody.vertices, pointer)) {
+    if (!Matter.Vertices.contains(Game.rock.rigidBody.vertices, pointer)) {
         return;
     }
 
-    // Transform from the click position in world coordinates to the object's local coordinates
-    let worldPos = new Phaser.Point(pointer.x, pointer.y);
-    let transformPos = Phaser.Point.subtract(worldPos, clickedObj.world);
-    let rotationMatrix = new Phaser.Matrix();
-    rotationMatrix.rotate(clickedObj.rotation);
-    let localPos = rotationMatrix.applyInverse(transformPos);
-    localPos.add(Game.rock.sprite.offsetX, Game.rock.sprite.offsetY);
-
     // Store the location where the rock was clicked
     Game.rockClicked = true;
-    Game.rockClickedPos = localPos;
+    Game.rockClickedPos = Game.rock.getLocalFromWorld(pointer.x, pointer.y);
     Game.forceLine = new Phaser.Line(pointer.x, pointer.y, pointer.x, pointer.y);
 }
 
@@ -140,17 +95,12 @@ Game.handleRockUnclicked = function(clickedObj, pointer) {
     }
 
     // Transform the stored rock clicked position into a world position
-    let localPos = new Phaser.Point(Game.rockClickedPos.x, Game.rockClickedPos.y);
-    localPos.subtract(Game.rock.sprite.offsetX, Game.rock.sprite.offsetY);
-    let rotationMatrix = new Phaser.Matrix();
-    rotationMatrix.rotate(Game.rock.sprite.rotation);
-    let transformPos = rotationMatrix.apply(localPos);
-    let worldPos = Phaser.Point.add(transformPos, Game.rock.sprite.position);
+    let worldPos = Game.rock.getWorldFromLocal(Game.rockClickedPos.x, Game.rockClickedPos.y);
     
     // Generate a force from the clicked position and the current cursor position
     let force = Phaser.Point.subtract(pointer, worldPos); 
     force.divide(1000, 1000); // scale the force
-    Matter.Body.applyForce(Game.rock.collisionBody, worldPos, force);
+    Matter.Body.applyForce(Game.rock.rigidBody, worldPos, force);
     
     Game.rockClicked = false;
     Game.forceLine = null;
---
+++ js/object.js
@@ -0,0 +1,91 @@
+
+/**
+* A class representing a basic game object containing a sprite and collision
+*/
+class GameObject {
+    /**
+     * Constructor
+     * @param {String} jsonFile - Name of the JSON file containing the description of this object's properties 
+     * @param {*} gameWorld  - The Gameworld where the object should be create
+     * @param {*} x - The X coordinate where the object should be created
+     * @param {*} y - The Y coordinate where the object should be created
+     */
+    constructor(jsonFile, gameWorld, x, y) {
+        // Load the object's JSON definition
+        this.definition = gameWorld.phaserGame.cache.getJSON(jsonFile);
+
+        // Initialize the object's sprite
+        if (this.definition.sprite !== null) {
+            this.sprite = gameWorld.phaserGame.add.sprite(x, y, this.definition.sprite);
+        }
+
+        // Initialize the object's collision
+        if (this.definition.collision !== null) {
+            if (this.definition.collision.verticies !== null) {
+                this.rigidBody = Matter.Bodies.fromVertices(x, y, Matter.Vertices.create(this.definition.collision.verticies));
+                Matter.World.add(gameWorld.physics.engine.world, this.rigidBody);
+            }
+
+            // Adjust the anchor of the sprite to the center of mass of the collision body
+            let centerOfMassOffset = Matter.Vector.sub(this.rigidBody.position, this.rigidBody.bounds.min);
+            let boundExtents = Matter.Vector.sub(this.rigidBody.bounds.max, this.rigidBody.bounds.min);
+            let anchorX = centerOfMassOffset.x / boundExtents.x;
+            let anchorY = centerOfMassOffset.y / boundExtents.y;
+            this.sprite.anchor.setTo(anchorX, anchorY);
+        }
+    }
+
+    /**
+     * Per frame update function for the object
+     */
+    update() {
+        // Update the sprite's transform to follow the physic's rigid body transform
+        this.sprite.x = this.rigidBody.position.x;
+        this.sprite.y = this.rigidBody.position.y;
+        this.sprite.rotation = this.rigidBody.angle;
+    }
+
+    /**
+     * Helper function to convert world coordinates into this object's local coordinate space
+     * @param {Number} worldX - The world X coordinate
+     * @param {Number} worldY - The world Y coordinate
+     * @return {Phaser.Point} The position of the point in local object space
+     */
+    getLocalFromWorld(worldX, worldY) {
+        // Transform the world position by the sprite's position
+        let transformPos = new Phaser.Point((worldX - this.sprite.world.x), (worldY - this.sprite.world.y));
+
+        // Create a rotation matrix and rotate the transform point
+        let rotationMatrix = new Phaser.Matrix();
+        rotationMatrix.rotate(this.sprite.rotation);
+        let localPos = rotationMatrix.applyInverse(transformPos);
+
+        // Apply the sprite's offset to the local position
+        localPos.add(Game.rock.sprite.offsetX, Game.rock.sprite.offsetY);
+
+        // Return the final local position
+        return localPos;
+    }
+
+    /**
+     * Helper function to convert from a point in local object space to world space
+     * @param {Number} localX - The local X coordinate
+     * @param {Number} localY - The local Y coordinate
+     * @return {Phaser.Point} The position of the point in world space
+     */
+    getWorldFromLocal(localX, localY) {
+        // Transform the local point by the sprite's offset
+        let transformPos = new Phaser.Point((localX - this.sprite.offsetX), (localY - this.sprite.offsetY));
+
+        // Create a rotation matrix from the sprite's current rotation value and rotate the transform point
+        let rotationMatrix = new Phaser.Matrix();
+        rotationMatrix.rotate(this.sprite.rotation);
+        let worldPos = rotationMatrix.apply(transformPos);
+        
+        // Add the sprite's translation to the final world position
+        worldPos.add(this.sprite.position.x, this.sprite.position.y);
+
+        // Return the final world position
+        return worldPos;
+    }
+}
